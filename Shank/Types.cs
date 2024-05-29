920cd6f3744a009ceba692db78f9a292e38a14a1
mendelsshop <mendelsshop@users.noreply.github.com>
2024-05-29 16:27:24 +0000

    Automated CSharpier Formatting

--- Shank/Types.cs
+++ Shank/Types.cs
@@ -1,12 +1,21 @@
 namespace Shank;
 
 public interface IType;
+
 public struct BooleanType : IType;
+
 public struct StringType : IType;
+
 public struct RealType : IType;
+
 public struct IntegerType : IType;
+
 public struct CharacterType : IType;
+
 public record struct EnumType(List<String> Variants) : IType;
+
 public record struct RecordType(Dictionary<String, IType> Fields, List<string> Generics) : IType;
+
 public record struct ArrayType(IType Inner) : IType;
+
 public record struct UnknownType(String TypeName, List<IType> TypeParameters) : IType;
