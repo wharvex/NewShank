fc2e357ef82816355656a87b250f6ffd4c890a68
Zain Ahmad <23ahmazain@gmail.com>
2024-06-11 17:38:49 -0400

    Unit Test File for Tran Parser

---
+++ TranUnitTests/ParserTests.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shank.Tran;
+using Tran;
+
+namespace TranUnitTests
+{
+    [TestClass]
+
+    public class ParserTests
+    {
+
+    }
+}
