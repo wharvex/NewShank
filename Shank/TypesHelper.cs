610b01308f7099879af6202d4731079a5a21df5b
wharvex <t.gudlewski@gmail.com>
2024-07-01 17:21:21 -0400

    helper methods
    Make helper methods to replace small visitors that didn't really agree with the
    spirit of a visitor.

---
+++ Shank/TypesHelper.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using Shank.ASTNodes;
+
+namespace Shank;
+
+public class TypesHelper
+{
+    public static Type? GetInnerType(Type t, VariableUsageNodeTemp vun)
+    {
+        switch (t)
+        {
+            case InstantiatedType it:
+                if (vun is VariableUsageMemberNode m)
+                {
+                    return it.GetMemberSafe(m.Right.Name, m);
+                }
+                throw new SemanticErrorException("Cannot dot into a " + vun.GetType(), vun);
+
+            case ArrayType at:
+                return at.Inner;
+            default:
+                return null;
+        }
+    }
+}
