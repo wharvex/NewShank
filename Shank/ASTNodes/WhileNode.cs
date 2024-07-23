1d438cd55b5afc8f654e9d2eefb0c8309049e175
WyattAlexander-001 <wyattbushman117@gmail.com>
2024-07-22 22:36:51 -0400

    adding XML to while and variableusageplainnode

--- Shank/ASTNodes/WhileNode.cs
+++ Shank/ASTNodes/WhileNode.cs
@@ -6,15 +6,31 @@ using Shank.IRGenerator;
 
 namespace Shank.ASTNodes;
 
+///<summary>
+///     Represents a 'while' loop node in the abstract syntax tree (AST).
+///</summary>
+///
 public class WhileNode : StatementNode
 {
+
+    ///<summary>
+    ///     Initializes a new instance of the <see cref="WhileNode"/> class with the specified expression and child statements.
+    ///</summary>
+    ///<param name="exp">The condition expression for the 'while' loop.</param>
+    ///<param name="children">The list of statements to be executed within the loop.</param>
+ 
     public WhileNode(ExpressionNode exp, List<StatementNode> children)
     {
         Expression = exp;
         Children = children;
     }
 
-    // Copy constructor for monomorphization
+    ///<summary>
+    ///     Copy constructor for creating a new instance of the <see cref="WhileNode"/> class with modified children.
+    ///</summary>
+    ///<param name="copy">The original <see cref="WhileNode"/> instance to copy.</param>
+    ///<param name="children">The list of statements to be executed within the loop.</param>
+
     public WhileNode(WhileNode copy, List<StatementNode> children)
     {
         Children = children;
@@ -23,8 +39,25 @@ public class WhileNode : StatementNode
         Expression = copy.Expression;
     }
 
+    ///<summary>
+    ///     Gets or sets the condition expression of the 'while' loop.
+    ///</summary>
+    ///
     public ExpressionNode Expression { get; set; }
+
+    ///<summary>
+    ///     Gets or sets the list of statements to be executed within the loop.
+    ///</summary>
+
     public List<StatementNode> Children { get; set; }
+
+    ///<summary>
+    ///     Returns an array of tokens representing the 'while' loop statement.
+    ///</summary>
+    ///<returns>
+    ///     An array containing the tokens of the 'while' loop statement.
+    ///     Throws a <see cref="SemanticErrorException"/> if the expression type is not supported.
+    ///</returns>
 
     public override object[] returnStatementTokens()
     {
@@ -50,6 +83,11 @@ public class WhileNode : StatementNode
 
         // return arr;
     }
+
+    ///<summary>
+    ///     Returns a string representation of the 'while' loop node.
+    ///</summary>
+    ///<returns>A string representing the 'while' loop node.</returns>
 
     public override string ToString()
     {
@@ -81,12 +119,33 @@ public class WhileNode : StatementNode
     //     // builder.PositionAtEnd(whileDone);
     // }
 
+
+    ///<summary>
+    ///     Accepts a statement visitor for processing this node.
+    ///</summary>
+    ///<param name="visit">The statement visitor to accept.</param>
+
     public override void Visit(StatementVisitor visit)
     {
         visit.Accept(this);
     }
 
+    ///<summary>
+    ///     Accepts a generic visitor for processing this node.
+    ///</summary>
+    ///<param name="v">The generic visitor to accept.</param>
+
     public override void Accept(Visitor v) => v.Visit(this);
+
+    ///<summary>
+    ///     Walks the node with a semantic analysis visitor, allowing the visitor
+    ///     to process and potentially modify the node and its children.
+    ///</summary>
+    ///<param name="v">The semantic analysis visitor that processes the node.</param>
+    ///<returns>
+    ///     <para>The resulting AST node if changes are made.</para>
+    ///     <para>Returns <c>null</c> if no changes are made.</para>
+    ///</returns>
 
     public override ASTNode? Walk(SAVisitor v)
     {
