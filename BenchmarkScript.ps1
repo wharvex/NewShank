f2baa03a1ba12e44ba42da67a0b587508bd3f5f5
Tim <81175852+wharvex@users.noreply.github.com>
2024-07-25 15:40:14 -0400

    more fibonacci benchmark work

--- BenchmarkScript.ps1
+++ BenchmarkScript.ps1
@@ -1,48 +1,56 @@
 function b {
-    "Python`n"
-    "Python`n" > BenchmarkScriptOutput.txt
+    "Running Python (Interpreted)"
+    "Running Python (Interpreted)`n" > BenchmarkScriptOutput.txt
 
     Measure-Command { python .\Shank\benchMark\Fibonacci\Fibonacci.py `
     *>> BenchmarkScriptOutput.txt }
 
-    "Shank Interpret No Vuop`n"
-    "Shank Interpret No Vuop`n" > BenchmarkScriptOutput.txt
+    "Running Shank (Interpreted) with No Vuop`n"
+    "Running Shank (Interpreted) with No Vuop`n" >> BenchmarkScriptOutput.txt
 
     Measure-Command { dotnet run Interpret .\Shank\benchMark\Fibonacci `
-    --project .\Shank\Shank.csproj >> BenchmarkScriptOutput.txt }
+    --project .\Shank\Shank.csproj *>> BenchmarkScriptOutput.txt }
 
-    "Shank Interpret Vuop`n"
-    "Shank Interpret Vuop`n" >> BenchmarkScriptOutput.txt
+    "Running Shank (Interpreted) with Vuop`n"
+    "Running Shank (Interpreted) with Vuop`n" >> BenchmarkScriptOutput.txt
 
     Measure-Command { dotnet run Interpret .\Shank\benchMark\Fibonacci `
-    --vuop-test --project .\Shank\Shank.csproj >> BenchmarkScriptOutput.txt }
+    --vuop-test --project .\Shank\Shank.csproj *>> BenchmarkScriptOutput.txt }
 
-    "Shank Compile No Vuop`n" >> BenchmarkScriptOutput.txt
+    "Compiling Shank with No Vuop`n" >> BenchmarkScriptOutput.txt
 
     dotnet run Compile .\Shank\benchMark\Fibonacci --print-ir --linker clang `
     -o .\TestCompileOutput\fibShankNoVuop.exe --project .\Shank\Shank.csproj `
     *>> BenchmarkScriptOutput.txt
 
-    "Shank Compile Vuop`n" >> BenchmarkScriptOutput.txt
+    "Compiling Shank with Vuop`n" >> BenchmarkScriptOutput.txt
 
     dotnet run Compile .\Shank\benchMark\Fibonacci --print-ir --vuop-test `
-    --linker clang -o .\TestCompileOutput\fibShankNoVuop.exe `
+    --linker clang -o .\TestCompileOutput\fibShankVuop.exe `
     --project .\Shank\Shank.csproj *>> BenchmarkScriptOutput.txt
 
-    "C Compile`n" >> BenchmarkScriptOutput.txt
+    "Compiling C`n" >> BenchmarkScriptOutput.txt
 
     clang .\Shank\benchMark\Fibonacci\Fibonacci.c `
     -o .\TestCompileOutput\fibC.exe *>> BenchmarkScriptOutput.txt
 
-    "C Compiled Executable`n" >> BenchmarkScriptOutput.txt
-    "C Compiled Executable`n"
+    "Running C (Compiled Executable)`n" >> BenchmarkScriptOutput.txt
+    "Running C (Compiled Executable)`n"
 
     Measure-Command { .\TestCompileOutput\fibC.exe `
     *>> BenchmarkScriptOutput.txt }
 
-    "Shank Compiled Executable`n" >> BenchmarkScriptOutput.txt
-    "Shank Compiled Executable`n"
+    "Running Shank (Compiled Executable) with No Vuop`n" `
+    >> BenchmarkScriptOutput.txt
+    "Running Shank (Compiled Executable) with No Vuop`n"
 
     Measure-Command { .\TestCompileOutput\fibShankNoVuop.exe `
+    *>> BenchmarkScriptOutput.txt }
+
+    "Running Shank (Compiled Executable) with Vuop`n" `
+    >> BenchmarkScriptOutput.txt
+    "Running Shank (Compiled Executable) with Vuop`n"
+
+    Measure-Command { .\TestCompileOutput\fibShankVuop.exe `
     *>> BenchmarkScriptOutput.txt }
 }
