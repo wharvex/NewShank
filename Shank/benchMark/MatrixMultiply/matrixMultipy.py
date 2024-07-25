242565ac06657ef086ecd0e99896b7b983db5dc8
mendelsshop <mendelsshop@gmail.com>
2024-07-25 12:39:51 -0400

    fixed matrix multiply benchmark

--- Shank/benchMark/MatrixMultiply/matrixMultipy.py
+++ Shank/benchMark/MatrixMultiply/matrixMultipy.py
@@ -1,12 +1,15 @@
 # TODO: make matrix class
-a = [[0 for x in range(100)] for x in range(100)]
-b = [[0 for x in range(100)] for x in range(100)]
-c = [[0 for x in range(100)] for x in range(100)]
+def initMatrix(seed):
+    return [[seed * (i + j) for j in range(585)] for i in range(585)]
+a = initMatrix(1)
+b = initMatrix(3)
+c = [[0 for x in range(585)] for x in range(585)]
+
 def matrixMultiply(a,b,c):
-    for i in range(100):
-        for j in range(100):
+    for i in range(585):
+        for j in range(585):
             sum = 0
-            for k in range(100):
+            for k in range(585):
                 sum = sum + a[i][k] * b[k][j]
         c[i][j] = sum
 matrixMultiply(a,b,c)
